ab5db7af23563b16d8b3fa1f0b87b5d2811acc64
Hansiyuan131 <422295068@qq.com>
2021-06-10 22:32:18 +0800

    demo init

---
+++ src/test/java/com/tt/threaddemo/ThreadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tt.threaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
